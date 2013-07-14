63943e6542f3635203a98ba66740cadd1e8c5718
Shyam Raghavan <shyam.raghavan.1@gmail.com>
2013-07-13 19:36:29 -0500

    wip

---
+++ src/sim/RunSim.java
@@ -0,0 +1,22 @@
+package sim;
+
+import form.GUIHandler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shyam
+ * Date: 7/13/13
+ * Time: 7:21 PM
+ */
+public class RunSim {
+
+    private static GUIHandler guiHandler;
+
+    public static void main (String args[]){
+
+        guiHandler = new GUIHandler();
+
+
+
+    }
+}
---
+++ src/util/Input.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shyam
+ * Date: 7/13/13
+ * Time: 7:35 PM
+ */
+public class Input {
+}
